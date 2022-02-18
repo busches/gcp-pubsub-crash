f8d936727b5bbac1aee2f61fdeed3b3d7b4dac8f
Scott Busche <swbusche@gmail.com>
2022-02-17 19:59:14 -0600

    Test with google's bom, still crashes

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+//	implementation(platform("com.google.cloud:libraries-bom:24.3.0"))
 }
 
 dependencyManagement {
